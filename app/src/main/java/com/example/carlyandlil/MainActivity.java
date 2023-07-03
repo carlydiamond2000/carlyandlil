35492338a4e07627228ddf160506a065827183da
lawrencediamond <carlydiamond123@icloud.com>
2023-07-03 17:17:10 +0300

    after watching class 15 technical video

--- app/src/main/java/com/example/carlyandlil/MainActivity.java
+++ app/src/main/java/com/example/carlyandlil/MainActivity.java
@@ -8,9 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken("847910857346-d4fq7vri0lgoklh5bjlr05aaftlld27i.apps.googleusercontent.com")
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Button signIn = findViewById(R.id.my_button50);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
 
         Button btn10 = findViewById(R.id.my_button10);
         btn10.setOnClickListener(new View.OnClickListener() {
@@ -45,4 +69,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Intent i = new Intent(MainActivity.this, Activity2.class);
+            i.putExtra("user", account);
+            startActivity(i);
+        } catch (ApiException e) {
+            // Handle sign-in failure
+        }
+    }
 }
+
+
+
